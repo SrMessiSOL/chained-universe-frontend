368dddcc7a04a667cd14796773d891b09c0f6f75
SrMessiSOL <srmessisol@gmail.com>
2026-04-03 22:39:55 -0300

    Update fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,21 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      buffer: 'buffer/',
+      process: 'process/browser',
+    },
+  },
+  define: {
+    global: 'globalThis',
+  },
+  optimizeDeps: {
+    include: ['buffer', 'process'],
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+    },
+  },
 })
